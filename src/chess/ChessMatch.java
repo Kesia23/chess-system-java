83827c7468f6b24ac307d809cfa58fc0ad62bba4
Kesia Silva <kesiasilvanunes@hotmail.com>
2019-10-22 14:38:30 -0400

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,31 +1,34 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
-public class ChessMatch {//partida de xadrez
+public class ChessMatch {// partida de xadrez
 	private Board board;
-	
+
 	public ChessMatch() {
-		board = new Board(8,8);//cria o tabulleiro
+		board = new Board(8, 8);// cria o tabulleiro
 		initialSetup();
 	}
-	
-	public ChessPiece[][] getPieces(){//retorna o tabuleiro
-	    ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-	    for(int i=0; i<board.getRows(); i++) {
-	    	for(int j=0; j<board.getColumns(); j++) {
-	    		mat[i][j] = (ChessPiece) board.piece(i,j);
-	    	}
-	    }
-	    return mat;
+
+	public ChessPiece[][] getPieces() {// retorna o tabuleiro
+		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for (int i = 0; i < board.getRows(); i++) {
+			for (int j = 0; j < board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) board.piece(i, j);
+			}
+		}
+		return mat;
 	}
-	
-	private void initialSetup() {//inicia a partida de xadrez, colocando as peças no tabuleiro
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
-		board.placePiece(new King(board, Color.WHITE), new Position(7,4));
+
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+
+	private void initialSetup() {// inicia a partida de xadrez, colocando as peças no tabuleiro
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 	}
 }
